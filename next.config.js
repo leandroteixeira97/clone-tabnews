578d2075faf4ef9bcd57f23c173740f86bfa809f
leandroteixeira97 <leandro.teixeira.sjc@gmail.com>
2023-08-29 20:00:19 -0300

    making the code prettier

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-let assetPrefix = ''
-let basePath = '/'
+let assetPrefix = "";
+let basePath = "/";
 
 if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
 
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
 }
