effb4d12f53dda821417778f45584f356b74ccb9
leandroteixeira97 <leandro.teixeira.sjc@gmail.com>
2024-03-15 13:49:53 -0300

    feat: adding environment variables

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(rquest, response) {
+import database from "../../../../infra/database.js";
+
+async function status(rquest, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result.rows);
   response.status(200).json({"nome": "Leandro"});
 }
 
